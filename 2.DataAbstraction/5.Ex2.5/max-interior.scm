9ab7a01c0984c552a76dce3f8993d8e0acfd01a9
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-10-12 19:27:08 +0900

    Modify Ex2.5 answer

--- 2.DataAbstraction/5.Ex2.5/max-interior.scm
+++ 2.DataAbstraction/5.Ex2.5/max-interior.scm
@@ -5,9 +5,6 @@
 (load "bintree")
 (load "leaf-sum")
 
-
-;; interior-node 数え上げ
-
 (define enumerate-interior-node
   (lambda (tree)
     (cases bintree tree
@@ -25,28 +22,3 @@
 		   (lambda (x y) (> (cadr x) (cadr y))))))
       (car (car l)))
     ))
-
-(define max-interior-1
-  (lambda (tree)
-    (define (max-interior-inner rslt t)
-      (let ((x (leaf-sum t)))
-	(cases bintree t
-	       (interior-node 
-		(key left right)
-		(let ((rettmp (if (or (> x (cadr rslt))
-				      (not (car rslt)))
-				  (list key x)
-				  rslt))
-		      (end (and (eq? 'leaf-node (car left))
-				(eq? 'leaf-node (car right)))))
-		  (let ((ret (if (eq? 'leaf-node (car left))
-				 rettmp
-				 (max-interior-inner rettmp left))))
-		    (if end
-			rettmp
-			(max-interior-inner ret right)))))
-	       (else rslt)))
-      )
-    (car (max-interior-inner '(#f 0) tree))
-    )
-  )
